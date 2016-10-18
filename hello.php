4a556fa1900e7eb94ecf227633d900862db4be3f
sanchitmehra <sanchitmehra52@gmail.com>
2016-10-18 02:07:34 -0500

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,13 +1,29 @@
 <?php
 $number=$_POST['9293298005'];
-$body=$_POST['Body'];
+$Body=$_POST['Body'];
 
     header("content-type: text/xml");
-  
+if ($_REQUEST['Body'] == 'Maths' || $_REQUEST['Body'] == 'maths')
+{
+	$message= "https://www.math.ksu.edu/~dbski/writings/further.pdf";
+}
+
+	else if($_REQUEST['Body'] == 'Science' || $_REQUEST['Body'] == 'science')
+	{
+		$message="science.pdf";
+		
+	}
+	else if($_REQUEST['Body'] == 'History' || $_REQUEST['Body'] == 'history')
+	{
+		$message="History.pdf";
+	}
+
+ 
 ?>
 <Response>
     <Message>
-Hello<?php echo $number?>.
-You said <?php echo $body?>
+Hello<?php echo $number?>
+<?php echo $message?>
+
 </Message>
 </Response>
