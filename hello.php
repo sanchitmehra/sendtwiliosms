cd11ac0633a859bc5d7f5888de219b29bf3ca73e
sanchitmehra <sanchitmehra52@gmail.com>
2016-10-18 14:44:29 -0500

    Update hello.php

--- hello.php
+++ hello.php
@@ -4,10 +4,21 @@ $body=$_POST['Body'];
 
     header("content-type: text/xml");
 
+if($_REQUEST['Body']=='Start'||$_REQUEST['Body']=='start'||$_REQUEST['Body']=='Restart'||$_REQUEST['Body']=='restart')
+	{       $message1="Please select one of the following options using a number or the subject name\n"; 
+		$message2="1. Maths\n";
+		$message3="2. Science\n";
+		$message4="3. History\n";
+	    $message5="4. English\n";	
+         $message="$message1$message2$message3$message4$message5";	
+	}
+
+
+
 ?>
 <Response>
     <Message>
-Hello  <?php echo $number?>.
+Hello  <?php echo $number?>
 <?php echo $body?> 
 
 
