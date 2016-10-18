00e076878fe957fde1a7de39aba9fb666dffb4a8
sanchitmehra <sanchitmehra52@gmail.com>
2016-10-18 00:49:25 -0500

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,9 +1,9 @@
 <?php
-$number=$_POST['9177465090'];
+$number=$_POST['9293298005'];
 $body=$_POST['Body'];
 
     header("content-type: text/xml");
-  //  echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+  
 ?>
 <Response>
     <Message>
