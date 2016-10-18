8563742ff388880d7f06cd77e2d6538d9051589f
sanchitmehra <sanchitmehra52@gmail.com>
2016-10-18 15:43:36 -0500

    Update hello.php

--- hello.php
+++ hello.php
@@ -10,7 +10,7 @@ if($_REQUEST['Body']=='Start'||$_REQUEST['Body']=='start'||$_REQUEST['Body']=='R
 		$message3="2. Science\n";
 		$message4="3. History\n";
 	    $message5="4. English\n";	
-         $message=$message1$message2$message3$message4$message5;	
+         $message="$message1$message2$message3$message4$message5";	
 	}
 
 if($_REQUEST['Body']=='1'||$_REQUEST['Body']=='Maths'||$_REQUEST['Body']=='maths')
@@ -20,7 +20,7 @@ if($_REQUEST['Body']=='1'||$_REQUEST['Body']=='Maths'||$_REQUEST['Body']=='maths
 		$message3="Text M7 for 7th grade notes\n";
 		$message4=" Text M8 for 8th grade notes";
 	    
-         $message=$message1$message2$message3$message4;	
+         $message="$message1$message2$message3$message4";	
 	}
 
 
@@ -30,7 +30,7 @@ if($_REQUEST['Body']=='M6')
 	$message1=" 1. 6th grade--- http://mrsmmartinsclassroom.weebly.com/uploads/1/8/5/7/18571084/6th_grade_workbook.pdf\n\n";
 	//$message2="2. 7th grade--- https://www.math.ksu.edu/~dbski/writings/further.pdf\n\n";
 	//$message3=" 8th grade--- http://www.glencoe.com/sites/common_assets/workbooks/math/pdf_workbook/m3ps.pdf\n";
-	  $message=$message1;	
+	  $message="$message1";	
 	}
 
 
